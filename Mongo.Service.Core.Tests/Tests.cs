9647d2cf827320960ded3e79515f104f659ea8a2
Rustam <rustambagaut@mail.ru>
2017-06-30 22:23:30 +0500

    Init solution

---
+++ Mongo.Service.Core.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Mongo.Service.Core.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Mongo.Service.Core/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mongo.Service.Core
+{
+    internal class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
