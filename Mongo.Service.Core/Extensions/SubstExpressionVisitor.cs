c98c1e4bf471f99e35ac8e37276f88c0e3cb7836
Rustam <kpytu3ha@yandex.ru>
2017-12-07 20:50:21 +0500

    One file -> one class

---
+++ Mongo.Service.Core/Extensions/SubstExpressionVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Mongo.Service.Core.Extensions
+{
+
+    internal class SubstExpressionVisitor : ExpressionVisitor
+    {
+        public Dictionary<Expression, Expression> Subst = new Dictionary<Expression, Expression>();
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            Expression newValue;
+            if (this.Subst.TryGetValue(node, out newValue))
+            {
+                return newValue;
+            }
+
+            return node;
+        }
+    }
+}
